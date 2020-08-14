33f2d908a3f72298af8b2b19f5c944db8862e556
abhishek7071 <38905469+abhishek7071@users.noreply.github.com>
2020-08-14 08:37:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import os
+from PIL import Image
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
@@ -9,4 +10,13 @@ chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 driver.get("https://www.google.com/")
 driver.get_screenshot_as_file("screenshot.png")
+driver.implicitly_wait(50)
+
+image.show() 
+
+driver.get_screenshot_as_file("screenshot.png")
+image = Image.open("image.png") 
+
+
 print(driver.page_source)
+
