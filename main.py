a592342a661a9dfd71a171a045b77fd8bd546a48
abhishek7071 <38905469+abhishek7071@users.noreply.github.com>
2020-08-14 18:21:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from fake_useragent import UserAgent
+chrome_options = Options()
 
 options = Options()
 ua = UserAgent()
