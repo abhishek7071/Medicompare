14c9fd605d6c5726b6fd7b6613ad0320fcba1c99
abhishek7071 <38905469+abhishek7071@users.noreply.github.com>
2020-09-28 00:48:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,10 +42,10 @@ time.sleep(10)
 f= open("a.txt","w+")
 f.write(driver.page_source)
 
-wait = WebDriverWait(driver, 20)
-wait.until(EC.presence_of_element_located((By.ID, "identifierNext"))).click()
+#wait = WebDriverWait(driver, 20)
+#wait.until(EC.presence_of_element_located((By.ID, "identifierNext"))).click()
 #driver.find_element_by_xpath(
-#driver.find_element_by_xpath('//*[@id="identifierNext"]').click()
+driver.find_element_by_xpath('//*[@id="identifierNext"]').click()
 #input = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="identifierNext"]/span/span')))
 #input.click()
 #time.sleep(3)
