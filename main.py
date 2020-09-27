f3acbf7dace4b1fd535c5dcb6695e4e04ed46e6f
abhishek7071 <38905469+abhishek7071@users.noreply.github.com>
2020-09-27 14:10:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 #import os
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 from PIL import Image
 import time
 from PIL import Image 
