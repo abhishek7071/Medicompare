273e63d3af99f5fc8407a0e80ab33bbe62404fd8
abhishek7071 <38905469+abhishek7071@users.noreply.github.com>
2020-10-03 09:37:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ from time import sleep
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
-from fake_useragent import UserAgent
 chrome_options = Options()
 
 options = Options()
